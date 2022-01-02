aa075a0f58a43499273d5eb6f1946b550b58c86c
wsmj5 <66855779+wsmj5@users.noreply.github.com>
2022-01-01 20:07:31 -0700

    Revert "Add files via upload"
    This reverts commit 6efedd22672ba24935221457dda5a5d7e7103ff1.

--- GBA.h
+++
@@ -1,9 +0,0 @@
-uint8_t* AGBmem = (uint8_t*)calloc(0xFFFFFFF, 1);
-
-#include "GBA/SROM.h"
-#include "GBA/EWRAM.h"
-#include "GBA/IWRAM.h"
-#include "GBA/IOR.h"
-#include "GBA/PRAM.h"
-#include "GBA/VRAM.h"
-#include "GBA/OAM.h"
